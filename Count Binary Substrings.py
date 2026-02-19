5fcaf45e25028b1337c9b6b91406cc8cf16085ab
classic__harsh <prajapatiharshit130@gamil.com>
2026-02-19 12:38:40 +0530

    Count Binary Substrings

---
+++ Count Binary Substrings.py
@@ -0,0 +1,17 @@
+def countBinarySubstrings(s: str) -> int:
+    prev_group = 0
+    curr_group = 1
+    result = 0
+
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            curr_group += 1
+        else:
+            result += min(prev_group, curr_group)
+            prev_group = curr_group
+            curr_group = 1
+
+    # last comparison
+    result += min(prev_group, curr_group)
+
+    return result
